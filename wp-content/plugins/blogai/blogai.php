63a0cd1194ad78e28dffaabada279bd5997cdcda
Geapen <theogilat@gmail.com>
2024-06-28 16:20:40 +0200

    My last commit

--- wp-content/plugins/blogai/blogai.php
+++ wp-content/plugins/blogai/blogai.php
@@ -308,7 +308,6 @@ function add_data_to_wp_posts() {
         $post_content = $data['article'];
         $post_title = $data['title'];
         $post_css = $data['cssStyles'];
-        $img_list = $data['imgList'];
 
         $new_post = array(
             'post_title' => $post_title,
@@ -333,7 +332,7 @@ function add_data_to_wp_posts() {
 }
 
 
-
+// not working
 /*function make_api_link() {
     global $wpdb, $api_url;
 
